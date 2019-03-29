2529befb5187f36e76006e0ddf94662838298a54
Michael Wonng <michael@wonng.com>
2019-03-29 21:49:13 +1100

    init test with test

---
+++ readme.md
@@ -0,0 +1,20 @@
+## Summary
+simulate search
+
+## Screebshot
+
+## Requirement
+
+node >= 10,
+npm or yarn
+
+## How to run
+
+
+## Production / Build
+
+`npm run build` or `yarn build`
+
+## Test
+
+`npm run test` or `yarn test`
---
+++ test/search.test.js
@@ -0,0 +1,32 @@
+import test from 'ava';
+
+
+test('search field id string', t => {
+  t.pass();
+});
+
+test('search field id bool', t => {
+  t.pass();
+});
+
+test('search field id array', t => {
+  t.pass();
+});
+
+test('result is not exist', t => {
+  t.pass();
+});
+
+test('searh field is not available', t => {
+  t.pass();
+});
+
+test('searh entity is not available', t => {
+  t.pass();
+});
+
+
+// List fields
+test('List available fields for search', t => {
+  t.pass();
+});
